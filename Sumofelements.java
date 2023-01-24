61ec1e3269cb2ce726cded5e0bece66812a4e716
Patil <dipali-laxman.patil@capgemini.com>
2023-01-24 14:34:27 +0530

    saved

---
+++ Sumofelements.java
@@ -0,0 +1,17 @@
+public class Sumofelements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int arr[]= {1,3,4,7,9};
+		int sum=0;
+		for(int i=0;i<5;i++)
+		{
+			sum=sum+arr[i];
+			
+		}
+		System.out.println("The sum is" +sum);
+	}
+
+}
